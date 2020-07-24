0a85e17c15fd03b10e81b2df0a2f254996afe76a
hung <Hung@Macbook-Pro.local>
2020-07-24 17:09:01 +0700

    observer

--- src/components/index/section3.js
+++ src/components/index/section3.js
@@ -6,32 +6,29 @@ import { faLongArrowAltRight, faLongArrowAltLeft } from '@fortawesome/free-solid
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
-//   {
-//     link: require('../../images/img-icon2.png'),
-//     header: 'Free Caliing Service',
-//     content: 'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
-//   },
-//   {
-//     link: require('../../images/img-icon1.png'),
-//     header: 'Free Caliing Service',
-//     content: 'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
-//   }
-// ].map((item, index) => (
-//   <div key={index} className="slide">
-//     <img alt="slide" src={item.link} />
-//     <h3>{item.header}</h3>
-//     <p>
-//       {item.content}
-//     </p>
-//   </div>
-// ))
+
+const data = [
+  { link: require('../../images/img-icon1.png') },
+  { link: require('../../images/img-icon2.png') },
+  { link: require('../../images/img-icon3.png') },
+].map((node, index) => (
+  <Slide key={index} index={index}>
+    <div className="slide">
+      <img alt="slide" src={node.link} />
+      <h3>Free Caliing Service</h3>
+      <p>
+        'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
+      </p>
+    </div>
+  </Slide>
+));
 
 const Section3 = () => {
   return (
     <CarouselProvider
       naturalSlideWidth={100}
       naturalSlideHeight={125}
-      totalSlides={6}
+      totalSlides={8}
       visibleSlides={3}
     >
       <div className="content">
@@ -46,60 +43,8 @@ const Section3 = () => {
         <div className="right">
           <div className="wrap-slide">
             <Slider>
-              <Slide index={0}>
-                <div className="slide">
-                  <img alt="slide" src={require('../../images/img-icon1.png')} />
-                  <h3>Free Caliing Service</h3>
-                  <p>
-                    'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
-                  </p>
-                </div>
-              </Slide>
-              <Slide index={1}>
-                <div className="slide">
-                  <img alt="slide" src={require('../../images/img-icon1.png')} />
-                  <h3>Free Caliing Service</h3>
-                  <p>
-                    'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
-                </p>
-                </div>
-              </Slide>
-              <Slide index={2}>
-                <div className="slide">
-                  <img alt="slide" src={require('../../images/img-icon1.png')} />
-                  <h3>Free Caliing Service</h3>
-                  <p>
-                    'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
-                </p>
-                </div>
-              </Slide>
-              <Slide index={3}>
-                <div className="slide">
-                  <img alt="slide" src={require('../../images/img-icon1.png')} />
-                  <h3>Free Caliing Service</h3>
-                  <p>
-                    'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
-                </p>
-                </div>
-              </Slide>
-              <Slide index={4}>
-                <div className="slide">
-                  <img alt="slide" src={require('../../images/img-icon1.png')} />
-                  <h3>Free Caliing Service</h3>
-                  <p>
-                    'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
-                </p>
-                </div>
-              </Slide>
-              <Slide index={5}>
-                <div className="slide">
-                  <img alt="slide" src={require('../../images/img-icon1.png')} />
-                  <h3>Free Caliing Service</h3>
-                  <p>
-                    'Direct mailing research development buyer iPad validation startup social proof learning curve user experience analytics'
-                </p>
-                </div>
-              </Slide>
+              {data}
+              {data}
             </Slider>
           </div>
         </div>
--- src/components/index/section4.js
+++ src/components/index/section4.js
@@ -1,28 +1,53 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import '../../styles/index/section4.scss'
 
-
 const data = {
-  content: 'There are many variations of passages of lorem ipsum available, but the majority have suffered alteration in some form, by injected humour lorem ipsum is simply free text in the'
+  content: `There are many variations of passages of lorem ipsum available,
+   but the majority have suffered alteration in some form, by injected humour 
+   lorem ipsum is simply free text in the`
 }
 
-const Section4 = () => (
-  <div className="container row">
-    <div className="left">
-      <img className="img-fluid" alt="fluid" src={require('../../images/img-fluid.png')} />
-      <img className="icon" alt="icon" src={require('../../images/img-circleicon1.png')} />
-    </div>
-    <div className="right">
-      <div className="mg-15">
-        <h6>01 – ADD NEW TASK</h6>
-        <h2>Everything starts with the task</h2>
-        <p>{data.content}</p>
-        <div className="btn-start-free-trial">
-          get started now
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2
+}
+
+function Section4() {
+  const imgFluid = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      console.group('Intersection')
+      for (const entry of entries) {
+        console.log('observe entry ...', entry.isIntersecting)
+      }
+      console.groupEnd()
+    }, observerOptions);
+
+    observer.observe(imgFluid.current)
+
+    return () => observer.unobserve(imgFluid.current)
+  }, [])
+
+  return (
+    <div className="container row" >
+      <div className="left">
+        <img className="img-fluid" name="1" ref={imgFluid} alt="fluid" src={require('../../images/img-fluid.png')} />
+        <img className="icon" alt="icon" src={require('../../images/img-circleicon1.png')} />
+      </div>
+      <div className="right">
+        <div className="mg-15">
+          <h6>01 – ADD NEW TASK</h6>
+          <h2>Everything starts with the task</h2>
+          <p>{data.content}</p>
+          <div className="btn-start-free-trial">
+            get started now
+        </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Section4;
