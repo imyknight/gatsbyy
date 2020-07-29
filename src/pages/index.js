46e3fbab200c5bf7d5f223c3c3e77a683cef4bd6
hung <Hung@Macbook-Pro.local>
2020-07-29 15:52:48 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import SEO from "../components/seo"
 import Layout from "../view/Layout/Layout"
 import Section1 from "../view/Sec1/Sec1"
 import Section2 from "../view/Sec2/Sec2"
+
 import Section3 from "../view/Sec3/Section3"
 import Section4 from "../view/Sec4/Section4"
 import Section6 from "../view/Sec6/Section6"
@@ -15,6 +16,9 @@ import Section7 from "../view/Sec7/Section7"
 import Section8 from "../view/Sec8/Section8"
 import Section9 from "../view/Sec9/Section9"
 import Section10 from "../view/Sec10/Section10"
+
+
+
 
 const IndexPage = ({ data }) => {
   return (
