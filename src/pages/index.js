797f2b287ab2e6a23e53e9d585c039b5ea1f7959
hung <Hung@Macbook-Pro.local>
2020-07-29 15:28:10 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,15 @@ import '../styles/styles.scss'
 import SEO from "../components/seo"
 import Layout from "../view/Layout/Layout"
 import Section1 from "../view/Sec1/Sec1"
-import Section2 from '../view/Sec2/Section2'
-import Section3 from '../view/Sec3/Section3'
-import Section4 from '../view/Sec4/Section4'
-import Section5 from '../view/Sec5/Section5'
-import Section6 from '../view/Sec6/Section6'
-import Section7 from '../view/Sec7/Section7'
-import Section8 from '../view/Sec8/Section8'
-import Section9 from '../view/Sec9/Section9'
-import Section10 from '../view/Sec10/Section10'
+import Section2 from "../view/Sec2/Section2"
+import Section3 from "../view/Sec3/Section3"
+import Section4 from "../view/Sec4/Section4"
+import Section5 from "../view/Sec5/Section5"
+import Section6 from "../view/Sec6/Section6"
+import Section7 from "../view/Sec7/Section7"
+import Section8 from "../view/Sec8/Section8"
+import Section9 from "../view/Sec9/Section9"
+import Section10 from "../view/Sec10/Section10"
 
 const IndexPage = ({ data }) => {
   return (
